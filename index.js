2edbb8fce391182048f93ab798ce128233ab17e7
Sai Uttarkar <usai7261@gmail.com>
2024-03-13 21:41:49 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 
 const Routes = require("./routes/Routes");
 require("./config/db")();
@@ -13,10 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({msg: "Hello, ATG!"});
-})
+	res.json({ msg: "Hello, ATG!", url: req.url });
+});
 app.use("/api", Routes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}@http://localhost:${PORT}`);
-})
+	console.log(`Server is running on port ${PORT}@http://localhost:${PORT}`);
+});
